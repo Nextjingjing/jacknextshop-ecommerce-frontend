bb204b8173341beaf2566cc9f5cdbad77766ec22
SNeXT <snext2004@gmail.com>
2025-04-09 12:24:07 +0700

    frist commit

---
+++ src/component/NavBar.tsx
@@ -0,0 +1,7 @@
+const NavBar = () => {
+  return (
+    <h1>TEST COMPONENT NAV FOR App.tsx</h1>
+  )
+};
+
+export default NavBar;
---
+++ src/page/Home.tsx
@@ -0,0 +1,7 @@
+const Home = () => {
+  return (
+    <div>I'm Home form ./page/Home.tsx I'm displayed by outlet in App.tsx</div>
+  )
+}
+
+export default Home
