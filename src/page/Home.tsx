3e4f48b776fa1e18c0424997842293329d287802
SNeXT <snext2004@gmail.com>
2025-04-10 01:38:54 +0700

    add SearchBar in Home.tsx

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,6 +1,9 @@
+import SearchBar from "../component/SearchBar"
+
 const Home = () => {
   return (
     <>
+      <SearchBar/>
       <div className="bg-blue-500 text-white p-6 rounded-lg shadow-lg">
         HOME PAGE 🎉
       </div>
