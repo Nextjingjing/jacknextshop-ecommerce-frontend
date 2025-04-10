6febab508a4749a2d096fcf9a6f34a4f1d63c728
jackpencwby <kajonyod.jack@gmail.com>
2025-04-10 14:20:34 +0700

    deleted component that don't nescessary

--- src/constants/navigation.tsx
+++ src/constants/navigation.tsx
@@ -1,16 +1,12 @@
 import Home from "../page/Home";
-import New from "../page/New";
 import About from "../page/About";
-import Report from "../page/Report";
 import Cart from "../page/Cart";
 import Notification from "../page/Notification";
 import Detail from "../page/Detail";
 
 const navigationForRouter = [
   { name: "หน้าแรก", href: "/", element: <Home /> },
-  { name: "ข่าวสาร", href: "/new", element: <New /> },
   { name: "เกี่ยวกับเรา", href: "/about", element: <About /> },
-  { name: "รายงานปัญหา", href: "/report", element: <Report /> },
   { name: "ตระกร้า", href: "/cart/:userId", element: <Cart /> },
   { name: "แจ้งเตือน", href: "/notification/:userId", element: <Notification /> },
   { name: "รายละเอียดสินค้า", href: "/product/:productId", element: <Detail /> },
@@ -18,9 +14,7 @@ const navigationForRouter = [
 
 const navigationForNavBar = [
   { name: "หน้าแรก", href: "/", element: <Home /> },
-  { name: "ข่าวสาร", href: "/new", element: <New /> },
   { name: "เกี่ยวกับเรา", href: "/about", element: <About /> },
-  { name: "รายงานปัญหา", href: "/report", element: <Report /> },
 ];
 
 export default navigationForRouter;
