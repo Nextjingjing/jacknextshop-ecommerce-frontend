113b488f6b363b79bdd702db337d2c48fe765107
jackpencwby <kajonyod.jack@gmail.com>
2025-04-12 13:27:10 +0700

    fix bug of product details and star review finish

--- src/component/detail/StarReview.tsx
+++ src/component/detail/StarReview.tsx
@@ -1,6 +1,37 @@
-function StarReview() {
+import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faStarHalfStroke } from "@fortawesome/free-solid-svg-icons/faStarHalfStroke";
+
+interface ComponentProp {
+  rating: number
+}
+
+const StarReview: React.FC<ComponentProp> = ({ rating }) => {
+  const stars = [];
+
+  while (rating > 0) {
+    if (rating - 1 >= 0) {
+      stars.push(<FontAwesomeIcon icon={faStar} size="sm" style={{ color: "#FFD43B", }} />)
+      rating--
+    }
+    else if (rating - 0.5 > 0) {
+      stars.push(<FontAwesomeIcon icon={faStar} size="sm" style={{ color: "#FFD43B", }} />)
+      rating -= 0.5
+    }
+    else if (rating - 0.5 == 0) {
+      stars.push(<FontAwesomeIcon icon={faStarHalfStroke} size="sm" style={{ color: "#FFD43B", }} />)
+      rating -= 0.5
+    }
+    else {
+      break
+    }
+  }
+
   return (
-    <div>StarReview</div>
+    <div>
+      {stars}
+    </div>
   )
 }
 
